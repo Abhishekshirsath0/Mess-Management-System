0c5113193f82986c6f99112660af3b5cf9b1342b
Abhishekshirsath0 <shirsathabhi512@gmail.com>
2026-09-01 21:17:31 +0530

    Optimzation

--- backend/Router/authRouter.js
+++ backend/Router/authRouter.js
@@ -7,7 +7,7 @@ const authRouter = express.Router();
 
 const limiter = rateLimit({
     windowMs: 60 * 60 * 1000, // 1 hour
-    limit: 10, // Limit each IP to 100 requests per `window` (here, per 1 hour).
+    limit: 20, // Limit each IP to 10 requests per `window` (here, per 1 hour).
     standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
     ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
